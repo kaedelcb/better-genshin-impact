16ef08d06b516b8094e68efda326fa4bdcd0b02b
kaedelcb <kaedelcb@qq.com>
2025-04-06 15:13:34 +0800

    测试

--- BetterGenshinImpact/GameTask/AutoDomain/AutoDomainTask.cs
+++ BetterGenshinImpact/GameTask/AutoDomain/AutoDomainTask.cs
@@ -89,7 +89,7 @@ public class AutoDomainTask : ISoloTask
     {
         _ct = ct;
 
-        Init();
+        Init();//LCB
         Notify.Event(NotificationEvent.DomainStart).Success("自动秘境启动");
 
         // 3次复活重试
