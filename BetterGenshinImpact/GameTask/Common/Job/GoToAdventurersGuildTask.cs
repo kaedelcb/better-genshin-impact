6e0087692efa0fd2bff08b07228498e34735ce74
kaedelcb <kaedelcb@qq.com>
2025-04-06 21:33:22 +0800

    LCB版本 一、自制标，用于外部其他软件进行判断，涉及文件： 1、修改BetterGenshinImpact\Core\Config\AllConfig.cs  2、增加BetterGenshinImpact\Core\Config\LCBauto.cs 3、BetterGenshinImpact\ViewModel\MaskWindowViewModel.cs LOG窗口串流显示标志
    二、一条龙的每周自动秘境改为7天可配置，并且亮起自制标（用于外部软件进行识别）,涉及文件:
    1、BetterGenshinImpact\Core\Config\OneDragonFlowConfig.cs 增加周一到周日的参数。
    2、BetterGenshinImpact\View\Pages\OneDragonFlowPage.xaml 软件页面修改，可配置周一到周日。
    3、BetterGenshinImpact\View\MainWindow.xaml 全自动选项默认打开状态
    4、BetterGenshinImpact\GameTask\Common\Job\CheckRewardsTask.cs 一条龙结束后打开自动钓鱼标记，并且不开书检查。
    
    一条龙开始钓鱼标和自制标都关闭，在未配置秘境名称时跳过执行：
    2、BetterGenshinImpact\Model\OneDragonTaskItem.cs
    领奖励后不开书：
    
    三、保留外部传入字符串接口备用，涉及文件：
    1、BetterGenshinImpact\Core\Script\Dependence\Model\SoloTask.cs
    
    四、删除尘歌壶API、删除烹饪标联动，涉及文件：
    1、BetterGenshinImpact\Core\Script\Dependence\Dispatcher.cs
    
    五、自动战斗最后切1号人物，万叶聚集材料后进行滚轮操作，删除万叶等待操作（官方优化了逻辑），涉及文件：
    1、BetterGenshinImpact\GameTask\AutoFight\AutoFightTask.cs
    
    ----七、领取每日奖励到达凯瑟琳后向前探索，后续准备删除，涉及文件：
        BetterGenshinImpact\GameTask\Common\Job\GoToAdventurersGuildTask.cs
    
    八，把那维莱特的转圈接口改为释放自动旋转吸取功能的API，涉及文件：
    1、BetterGenshinImpact\GameTask\Macro\TurnAroundMacro.cs
    2、BetterGenshinImpact\ViewModel\Pages\HotKeyPageViewModel.cs  快捷方式绑定页面修改LCB自制调用旋转吸取API显示。
    
    九、跑图火战斗过程角色识别失败改为不退出，涉及文件：
    1、BetterGenshinImpact\GameTask\RunnerContext.cs
    
    十、配置组开始关闭自动钓鱼标记，涉及文件：
    1、BetterGenshinImpact\Service\ScriptService.cs

--- BetterGenshinImpact/GameTask/Common/Job/GoToAdventurersGuildTask.cs
+++ BetterGenshinImpact/GameTask/Common/Job/GoToAdventurersGuildTask.cs
@@ -155,16 +155,6 @@ public class GoToAdventurersGuildTask
             EndAction = region => Bv.FindFAndPress(region, text: this.catherineLocalizedString)
         };
         await pathingTask.Pathing(task);
-
-        //===========向前走备份不生效，看情况====LCB
-        Logger.LogInformation("到达，向前寻找按钮");
-        Simulation.SendInput.Keyboard.KeyDown(User32.VK.VK_A);
-        Simulation.SendInput.Keyboard.KeyDown(User32.VK.VK_W);
-        await Delay(100, ct);
-        Simulation.SendInput.Keyboard.KeyUp(User32.VK.VK_A);
-        Simulation.SendInput.Keyboard.KeyUp(User32.VK.VK_W);
-        //===========向前走备份不生效====LCB
-        
         await Delay(600, ct);
 
         const int retryTalkTimes = 3;
