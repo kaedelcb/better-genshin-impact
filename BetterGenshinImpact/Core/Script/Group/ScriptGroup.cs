7b4f662d4869e32c29928876c48049c6f87063e3
秋云 <physligl@gmail.com>
2026-08-23 02:03:09 +0800

    fix: 原子保存脚本配置组 (#3502)
    Co-authored-by: 辉鸭蛋 <huiyadanli@gmail.com>
    
    (cherry picked from commit bf13dbd47527113a71ef6b4af572b6d52cfd4f58)

--- BetterGenshinImpact/Core/Script/Group/ScriptGroup.cs
+++ BetterGenshinImpact/Core/Script/Group/ScriptGroup.cs
@@ -1,8 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.Text.Json;
+using System.IO;
+using System.Text;
 using BetterGenshinImpact.Service;
 using CommunityToolkit.Mvvm.ComponentModel;
 using JsonSerializer = System.Text.Json.JsonSerializer;
@@ -13,6 +15,8 @@ namespace BetterGenshinImpact.Core.Script.Group;
 /// </summary>
 public partial class ScriptGroup : ObservableObject
 {
+    private static readonly Encoding Utf8WithoutBom = new UTF8Encoding(false);
+
     public int Index { get; set; }
 
     [ObservableProperty]
@@ -45,6 +49,26 @@ public partial class ScriptGroup : ObservableObject
     public string ToJson()
     {
         return JsonSerializer.Serialize(this, ConfigService.JsonOptions);
+    }
+
+    public void WriteToFileAtomically(string filePath)
+    {
+        var json = ToJson();
+        var fullPath = Path.GetFullPath(filePath);
+        var directory = Path.GetDirectoryName(fullPath)
+                        ?? throw new ArgumentException("配置组文件路径无效", nameof(filePath));
+        Directory.CreateDirectory(directory);
+
+        var tempPath = Path.Combine(directory, $".{Path.GetFileName(fullPath)}.{Guid.NewGuid():N}.tmp");
+        try
+        {
+            File.WriteAllText(tempPath, json, Utf8WithoutBom);
+            File.Move(tempPath, fullPath, overwrite: true);
+        }
+        finally
+        {
+            File.Delete(tempPath);
+        }
     }
 
     public static ScriptGroup FromJson(string json)
--- BetterGenshinImpact/ViewModel/Pages/ScriptControlViewModel.cs
+++ BetterGenshinImpact/ViewModel/Pages/ScriptControlViewModel.cs
@@ -2293,7 +2293,7 @@ public partial class ScriptControlViewModel : ViewModel
             }
 
             var file = Path.Combine(ScriptGroupPath, $"{scriptGroup.Name}.json");
-            File.WriteAllText(file, scriptGroup.ToJson());
+            scriptGroup.WriteToFileAtomically(file);
         }
         catch (Exception e)
         {
