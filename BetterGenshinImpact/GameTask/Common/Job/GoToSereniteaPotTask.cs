3c01300c82d989e46b80ce694eeaf4d95be93ba5
茶包 <kaedelcb@qq.com>
2026-09-07 21:11:41 +0800

    尘歌壶点击住宅优化

--- BetterGenshinImpact/GameTask/Common/Job/GoToSereniteaPotTask.cs
+++ BetterGenshinImpact/GameTask/Common/Job/GoToSereniteaPotTask.cs
@@ -127,13 +127,13 @@ internal class GoToSereniteaPotTask
                 Logger.LogInformation("领取尘歌壶奖励:{text}", "住宅图标未找到，调整地图缩放至2。");
                 //移动鼠标到地图中心，尝试缩放地图
                 Simulation.SendInput.Mouse.MoveMouseTo(1, 1);
-                await Task.Delay(2000, ct);
+                await Task.Delay(1000, ct);
                 await new Core.Script.Dependence.Genshin().SetBigMapZoomLevel(2.5-i*0.2);//尝试缩放地图
-                await Task.Delay(2000, ct);
+                await Task.Delay(3000, ct);
             }
             else
             {
-                await Delay(100, ct);
+                await Delay(500, ct);
                 sereniteaPotHomeIcon.Click();
                 await Delay(500, ct);
                 break;
